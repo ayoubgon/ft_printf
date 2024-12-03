8bfb3731610126aa81fd2c9db9ebcae360f89204
ayoub dehbi <77251921+ayoubgon@users.noreply.github.com>
2024-12-03 19:59:11 +0100

    Add files via upload

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: adehbi <adehbi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 11:46:50 by adehbi            #+#    #+#             */
-/*   Updated: 2024/11/22 17:00:33 by adehbi           ###   ########.fr       */
+/*   Updated: 2024/12/03 19:56:14 by adehbi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,24 @@ int	ft_printf(const char *str, ...)
 	i = 0;
 	while (str[i])
 	{
-		if (str[i] == '%')
+		if (str[i] == '%' && str[i + 1])
 		{
 			i++;
-			len += ft_putfun(arg, str[i++]);
+			len += ft_putfun(arg, str[i]);
 		}
-		else
-			len += ft_putchar(str[i++]);
+		else if (str[i] != '%')
+			len += ft_putchar(str[i]);
+		i++;
 	}
 	va_end(arg);
 	return (len);
 }
+// #include <libc.h>
+
+// int main()
+// {
+// 	int i = ft_printf("%\n");
+// 	printf("\n");
+// 	int j = printf("%\n");
+// 	printf("\n dyaleq %d | sys %d ",i,j);
+// }
